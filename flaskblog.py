b1180a38b87018d87b9b19e815c11724873d120f
mamboedwin <edwinmambo33@gmail.com>
2019-10-27 13:01:47 +0300

    ...format...

--- flaskblog.py
+++ flaskblog.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-	return  <h1>Welcome, to the world of Flask! Enjoy!</h1>
+    return < h1 > Welcome, to the world of Flask! Enjoy!< /h1 >
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
