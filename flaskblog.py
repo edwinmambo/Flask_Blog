426a7a68f0413effba8be188dd9cb1e8a366b7fe
mamboedwin <edwinmambo33@gmail.com>
2019-10-30 23:21:56 +0300

    mmh..

--- flaskblog.py
+++ flaskblog.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return < h1 > Welcome, to the world of Flask! Enjoy!< /h1 >
+    return "<h1>Welcome, to the world of Flask! Enjoy!</h1>"
 
 
 if __name__ == '__main__':
